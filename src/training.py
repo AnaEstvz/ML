970d46805002fda98ca59054708fa46473c872c8
ana.estvzz@gmail.com <ana.estvzz@gmail.com>
2024-04-19 18:45:33 +0200

    subida

---
+++ src/training.py
@@ -0,0 +1,7 @@
+# Importamos librerias necesarias
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import RandomizedSearchCV
+from xgboost import XGBClassifier
+
+# Leer csv definitvo con feature engineering
