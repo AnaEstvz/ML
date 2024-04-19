6f8e20442a59f5e49585c721e44a983d8a9b0735
ana.estvzz@gmail.com <ana.estvzz@gmail.com>
2024-04-19 19:18:43 +0200

    archivos pyv

---
+++ src/evaluation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pickle 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# Importar el modelo con pickle 
+filename = '../models_class/final_model2.pkl'
+
+with open(filename, "rb") as archivo_entrada:
+    final_model = pickle.load(archivo_entrada)
+
+# Leer df test para hacer predicciones
+df1 = pd.read_csv('../data/test/test2.csv')
+
+# Datos test
+X_test = df1[['popularity',
+       'acousticness',
+       'instrumentalness',
+        'valence',  'loudness_category_numerica2',
+       'speech_cat_num2',
+        'energy_cat_num2']]
+y_test = df1["danceability_category_encoded"]
+
+# Hacemos predicciones 
+
+pred_test = final_model.predict(X_test)
+pred_proba_test = final_model.predict_proba(X_test)
+
+print("Resultados sobre TEST")
+print('Accuracy:', accuracy_score(y_test, pred_test))
+print('Precision:', precision_score(y_test, pred_test))
+print('Recall:', recall_score(y_test, pred_test))
+print('F1 Score:', f1_score(y_test, pred_test))
+print('Confusion Matrix:\n', confusion_matrix(y_test, pred_test))
--- src/training.py
+++ src/training.py
@@ -1,7 +1,72 @@
 # Importamos librerias necesarias
 import pandas as pd
+import pickle
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import RandomizedSearchCV
 from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
 
 # Leer csv definitvo con feature engineering
+music = pd.read_csv('../data/processed/music_final_class1.csv')
+
+# Dividir en train y test
+train_df, test_df = train_test_split(music, test_size=0.20, random_state=42 )
+
+# Guardar en csv
+train_df.to_csv('../data/train/train2.csv', index= False)
+test_df.to_csv('../data/test/test2.csv', index= False)
+
+# Abrir train para entrenar mi modelo
+df = pd.read_csv('../data/train/train2.csv')
+
+# Datos de entrenamiento
+
+X_train = df[['popularity',
+       'acousticness',
+       'instrumentalness',
+        'valence',  'loudness_category_numerica2',
+       'speech_cat_num2',
+        'energy_cat_num2']]
+y_train = df["danceability_category_encoded"]
+
+# Mejor modelo entrenado
+
+steps = [('classifier', XGBClassifier(random_state=42))]
+pipeline = Pipeline(steps)
+
+
+param_dist = {
+    'classifier__n_estimators': [100, 150, 200],
+    'classifier__max_depth': [9, 10, 15],
+    'classifier__learning_rate': [0.04,0.05],
+    'classifier__subsample': [0.8, 0.9],
+    'classifier__colsample_bytree': [0.8, 0.9],
+    'classifier__gamma': [0, 0.1],
+    'classifier__reg_alpha': [0.05, 0.3,0.5],
+    'classifier__reg_lambda': [ 0.3,0.4]
+}
+
+
+random_search = RandomizedSearchCV(pipeline, param_distributions=param_dist, cv=4, n_iter=30, n_jobs=-1, random_state=42, verbose=2)
+random_search.fit(X_train, y_train)
+
+
+final_model1 = random_search.best_estimator_
+
+# Obtener los mejores resultados
+best_score = random_search.best_score_
+best_params = random_search.best_params_
+best_estimator = random_search.best_estimator_
+
+# Entrenar el mejor modelo con los datos de entrenamiento
+final_model1.fit(X_train, y_train)
+
+print("Best Score:", best_score)
+print("Best Parameters:", best_params)
+print("Best Estimator:", best_estimator)
+
+# Guardar modelo con pickle
+filename = '../models_class/final_model2.pkl'
+with open(filename, 'wb') as archivo_salida:
+    pickle.dump(final_model1, archivo_salida)
+
